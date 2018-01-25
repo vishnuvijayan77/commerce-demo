541c0b86c07d6fa6d5a40bfdd050c0533ffc6831
Hubbs <mhubbard@acromediainc.com>
2018-01-25 12:18:24 -0800

    Initial UH Event product template files and styling

---
+++ web/themes/custom/commerce_2_demo/js/custom/plus/uh-event.js
@@ -0,0 +1,48 @@
+/**
+ * Custom scripts (for UH+).
+ */
+
+(function ($, Drupal) {
+
+  /**
+   * UH Event - Scripts w/o AJAX events.
+   *
+   * This section is for general scripts that no not involve any AJAX events.
+   */
+
+  // Move title into first paragraph
+  $('.product--full--uh-event .page-title').prependTo('.paragraph--type--2-column-image-content__content:first');
+
+  // Paragraph buttons.
+  $('.product--full--uh-event .paragraph a').each(function (event) {
+    // If Register Now link.
+    if ($(this).text().toLowerCase() === 'register now') {
+      // Add highlight class.
+      $(this).addClass('highlight');
+      // Add click event.
+      $(this).click(function (event) {
+        // Prevent link default.
+        event.preventDefault();
+        // Scroll to add to cart form.
+        $('html, body').animate({
+          scrollTop: $(".product__event").offset().top -200
+        }, 700);
+      });
+    }
+  });
+
+  /**
+   * UH Event - Scripts w/ AJAX events.
+   *
+   * This section is for general scripts that involve AJAX events.
+   */
+
+  Drupal.behaviors.uhrecurring = {
+    attach: function(context, settings) {
+
+      // Do stuff here.
+
+    }
+  };
+
+})(jQuery, Drupal);
